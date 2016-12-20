e5c5df265f7981d4589f3b901a7cf75f7ba96a16
Jon Wood <woodjon@gmail.com>
2016-12-20 10:04:38 -0500

    Get initialize and template test file working

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
 #! /usr/bin/env node
+var fs = require('fs');
+
 var args = process.argv.splice(2);
+var e2eDir = "./e2e";
+var defaultTest = "./e2e/test.spec.js";
 
 switch(args[0]) {
   case "init":
+    if(!fs.existsSync(e2eDir)) {
+      fs.mkdirSync(e2eDir);
+    }
+
+    console.log(process.cwd());
+    console.log(__dirname);
+
+    var template = fs.readFileSync(__dirname + "/template.js", "UTF-8");
+
+    fs.writeFileSync(defaultTest, template);
+
     console.log("initialize");
     break;
   case "add":
     console.log("add new spec file");
 
+    // Add default spec file - test.spec.js (check if file already 
+    // exists and change to test2.spec.js if it does)
+
     if(args[1]) {
       console.log("...with name " + args[1]);
+
+      // Add spec file with specified name
     }
 
     break;
---
+++ template.js
@@ -0,0 +1,41 @@
+const Application = require("spectron").Application;
+const path = require('path');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+
+var electronPath = path.join(__dirname, '..', 'node_modules', '.bin', 'electron');
+
+if (process.platform === 'win32') {
+    electronPath += '.cmd';
+}
+
+var appPath = path.join(__dirname, '..');
+
+var app = new Application({
+    path: electronPath,
+    args: [appPath]
+  });
+
+global.before(function () {
+  chai.should();
+  chai.use(chaiAsPromised);
+});
+
+describe('Test Example', function () {
+  beforeEach(function (done) {
+      app.start().then(function() { 
+        done(); 
+      } 
+    );
+  });
+
+  afterEach(function (done) {
+    app.stop().then(function() {
+      done();
+    });
+  });
+
+  it('opens a window', function () {
+    page.getWindowCount().should.eventually.equal(page.windowCount);
+  });
+});
