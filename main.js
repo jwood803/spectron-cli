f31a96af596ee519531a8c03b3503093aa11f137
Jon Wood <woodjon@gmail.com>
2016-12-20 10:38:03 -0500

    Some 'add' functionality

--- main.js
+++ main.js
@@ -5,15 +5,14 @@ var args = process.argv.splice(2);
 var e2eDir = "./e2e";
 var defaultTest = "./e2e/test.spec.js";
 var defaultPage = "./e2e/test.page.js";
+var testTemplate = fs.readFileSync(__dirname + "/template.js", "UTF-8");
+var pageTemplate = fs.readFileSync(__dirname + "/template.page.js", "UTF-8");
 
 switch(args[0]) {
   case "init":
     if(!fs.existsSync(e2eDir)) {
       fs.mkdirSync(e2eDir);
     }
-
-    var testTemplate = fs.readFileSync(__dirname + "/template.js", "UTF-8");
-    var pageTemplate = fs.readFileSync(__dirname + "/template.page.js", "UTF-8");
 
     fs.writeFileSync(defaultTest, testTemplate);
     fs.writeFileSync(defaultPage, pageTemplate);
@@ -22,14 +21,34 @@ switch(args[0]) {
     break;
   case "add":
     console.log("add new spec file");
+    var specFile = e2eDir + "/test.spec.js";
 
-    // Add default spec file - test.spec.js (check if file already 
-    // exists and change to test2.spec.js if it does)
+    if(!fs.existsSync(e2eDir)) {
+      fs.mkdirSync(e2eDir);
+    }
+
+    if(fs.existsSync(specFile)) {
+      fs.writeFileSync("./e2e/test2.spec.js", testTemplate);
+      fs.writeFileSync("./e2e/test2.page.js", pageTemplate);
+    }
+    else {
+      fs.writeFileSync(defaultTest, testTemplate);
+      fs.writeFileSync(defaultPage, pageTemplate);
+    }
 
     if(args[1]) {
       console.log("...with name " + args[1]);
+      var testName = args[1];
+      var testFile = e2eDir + "/" + testName + ".spec.js";
+      var testPage = e2eDir + "/" + testName + ".page.js";
+      
+      if(!fs.existsSync(testFile)) {
+        fs.writeFileSync(testFile, testTemplate);
+      }
 
-      // Add spec file with specified name
+      if(!fs.existsSync(testPage)) {
+        fs.writeFileSync(testPage, pageTemplate);
+      }
     }
 
     break;
